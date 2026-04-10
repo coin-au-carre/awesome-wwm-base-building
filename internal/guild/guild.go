d1faaaa6d38a30114d37de6d9664a0b45e03334a
Florian Dang <florian.dang@olrea.fr>
2026-04-10 03:41:45 +0200

    sections guild

--- internal/guild/guild.go
+++ internal/guild/guild.go
@@ -1,18 +1,24 @@
 package guild
 
+type ScreenshotSection struct {
+	Label       string   `json:"label,omitempty"`
+	Screenshots []string `json:"screenshots"`
+}
+
 type Guild struct {
-	ID               string   `json:"id,omitempty"`
-	Name             string   `json:"name"`
-	GuildName        string   `json:"guildName,omitempty"`
-	Builders         []string `json:"builders"`
-	Tags             []string `json:"tags,omitempty"`
-	DiscordThread    string   `json:"discordThread"`
-	BuilderDiscordID string   `json:"builderDiscordId,omitempty"`
-	Lore             string   `json:"lore,omitempty"`
-	WhatToVisit      string   `json:"whatToVisit,omitempty"`
-	Score            int      `json:"score"`
-	Note             string   `json:"note,omitempty"`
-	CoverImage       string   `json:"coverImage,omitempty"`
-	Screenshots      []string `json:"screenshots,omitempty"`
-	Videos           []string `json:"videos,omitempty"`
+	ID                 string              `json:"id,omitempty"`
+	Name               string              `json:"name"`
+	GuildName          string              `json:"guildName,omitempty"`
+	Builders           []string            `json:"builders"`
+	Tags               []string            `json:"tags,omitempty"`
+	DiscordThread      string              `json:"discordThread"`
+	BuilderDiscordID   string              `json:"builderDiscordId,omitempty"`
+	Lore               string              `json:"lore,omitempty"`
+	WhatToVisit        string              `json:"whatToVisit,omitempty"`
+	Score              int                 `json:"score"`
+	Note               string              `json:"note,omitempty"`
+	CoverImage         string              `json:"coverImage,omitempty"`
+	Screenshots        []string            `json:"screenshots,omitempty"`
+	ScreenshotSections []ScreenshotSection `json:"screenshotSections,omitempty"`
+	Videos             []string            `json:"videos,omitempty"`
 }
