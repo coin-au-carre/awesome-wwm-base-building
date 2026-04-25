b068c06c53180f411c6fd88b4d781fc121b8c4c3
Florian Dang <florian.coin@gmail.com>
2026-04-25 13:43:11 +0200

    bot: send dm

--- cmd/send/main.go
+++ cmd/send/main.go
@@ -14,6 +14,7 @@ import (
 
 func main() {
 	channel := flag.String("channel", "", "discord channel ID to post in (overrides BOT_CHANNEL_ID)")
+	user := flag.String("user", "", "discord user ID to DM directly")
 	image := flag.String("image", "", "path to an image file to attach")
 	flag.Parse()
 
@@ -23,20 +24,32 @@ func main() {
 
 	token := cmdutil.RequireEnv("RUBY_BOT_TOKEN")
 
-	channelID := *channel
-	if channelID == "" {
-		channelID = cmdutil.RequireEnv("RUBY_CHANNEL_ID")
-	}
-
-	text := flag.Arg(0)
-	if text == "" && *image == "" {
-		slog.Error("provide a message and/or -image path")
+	if *user != "" && *channel != "" {
+		slog.Error("use -user or -channel, not both")
 		os.Exit(1)
 	}
 
 	s, err := discordgo.New("Bot " + token)
 	if err != nil {
 		slog.Error("creating session", "err", err)
+		os.Exit(1)
+	}
+
+	channelID := *channel
+	if *user != "" {
+		ch, err := s.UserChannelCreate(*user)
+		if err != nil {
+			slog.Error("opening DM channel", "user", *user, "err", err)
+			os.Exit(1)
+		}
+		channelID = ch.ID
+	} else if channelID == "" {
+		channelID = cmdutil.RequireEnv("RUBY_CHANNEL_ID")
+	}
+
+	text := flag.Arg(0)
+	if text == "" && *image == "" {
+		slog.Error("provide a message and/or -image path")
 		os.Exit(1)
 	}
 
